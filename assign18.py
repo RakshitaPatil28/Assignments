9879bbf93bc589df298b525846e6c24f354542e9
RakshitaPatil28 <patilrakshita28@gmail.com>
2026-01-09 16:33:24 +0530

    Add files via upload

---
+++ assign18.py
@@ -0,0 +1,26 @@
+#list
+animal=["Dog","Cat","Cow","Buffalo","Monkey"]
+print(animal)
+print(animal[1])
+print(animal[-3])
+#slicing
+print(animal[2:4])
+print(animal[-4:-2])
+print(animal[1:5:2])
+#methods of list
+animal.append("Donkey")
+print(animal)
+animal.insert(2,"Lion")
+print(animal)
+animal.remove("Cow")
+print(animal)
+animal.sort()
+print(animal)
+animal.reverse()
+print(animal)
+print(len(animal))
+animal.pop()
+print(animal)
+#del animal
+#print(animal)
+
